48f00bfa07daecff9462f9553f308350feb849da
Prakash Sejwani <prakashsejwani@gmail.com>
2022-07-26 16:49:14 +0530

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+        "grid": "autoplace"
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+  },
+};
